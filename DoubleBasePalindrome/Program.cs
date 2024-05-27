dfb09bb2aa9d8d52f815065b1cb8c1c17b53b5ec
Eason Shao <eason.syc@icloud.com>
2024-05-27 11:58:44 +0100

    init repo.

---
+++ DoubleBasePalindrome/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Security.AccessControl;
+
+internal class Program
+{
+    static bool IsPalindrome(string str)
+    {
+        return str == string.Join("", str.ToCharArray().Reverse().ToArray());
+    }
+    static void Main()
+    {
+        List<int> palindromeNumber = [];
+
+        for (int i = 0; palindromeNumber.Count < 20; ++i)
+        {
+            if (IsPalindrome(i.ToString()) && IsPalindrome(Convert.ToString(i, 2)))
+            {
+                palindromeNumber.Add(i);
+            }
+        }
+
+        Console.WriteLine($"{"Denary", -10} Binary");
+
+        foreach (int num in palindromeNumber)
+        {
+            Console.WriteLine($"{num, -10} {Convert.ToString(num, 2)}");
+        }
+    }
+}
